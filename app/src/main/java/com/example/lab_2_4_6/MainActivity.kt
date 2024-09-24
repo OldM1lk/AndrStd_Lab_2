0976ece4a52763637f4233032866a2713d76fa7f
OldM1lk <MrlaimWin@gmail.com>
2024-09-24 11:42:28 +0700

    Added new branch and code for task2

--- app/src/main/java/com/example/lab_2_4_6/MainActivity.kt
+++ app/src/main/java/com/example/lab_2_4_6/MainActivity.kt
@@ -5,6 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +19,25 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val editText: EditText = findViewById(R.id.edit_text)
+        editText.setText("")
+        val buttonLog: Button = findViewById(R.id.button_log)
+
+        buttonLog.setOnClickListener {
+            val text = editText.text.toString()
+
+            Log.v("From EditText: ", text)
+        }
+
+        Timber.plant(Timber.DebugTree())
+        val buttonTimber: Button = findViewById(R.id.button_timber)
+
+        buttonTimber.setOnClickListener {
+            val text = editText.text.toString()
+
+            Timber.v(text)
         }
     }
 }
